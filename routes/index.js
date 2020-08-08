881dd081418cadde2c6528bcf4adfe9d3d27e52b
jeremie-dalpe <jeremie.dalpe@usherbrooke.ca>
2020-08-08 16:24:27 -0400

    added start stop and implemented new front page

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,45 @@
 var express = require('express');
+var dateFormat = require('dateformat');
 var router = express.Router();
+
+var lastSave = new Date();
+var currentData = null;
+
+var gotStartRequest = false;
+var gotStopRequest = false;
 
 router.get('/', function (req, res) {
   var db = req.db;
   var collection = db.get('plant_iot_log');
 
-  var currentData = null
-  collection.findOne({}, { sort: { lastPumpActivation: -1 } }, function (e, docs) {
-    currentData = docs;
-    console.log(docs);
+  if (currentData == null) {
+    console.log("current data is null");
+    collection.findOne({}, { sort: { lastPumpActivation: -1 } }, function (e, docs) {
+      currentData = docs;
+      console.log(docs);
+
+      if (currentData.isPumpOn == true) {
+        currentData.isPumpOn = "En marche";
+      } else if (currentData.isPumpOn == false) {
+        currentData.isPumpOn = "À l'arrêt";
+      }
+
+      var myDate = new Date(currentData.lastPumpActivation);
+      currentData.lastPumpActivation = dateFormat(myDate, "h:MM:ss, dddd, mmmm dS");
+
+      res.render('home', {
+        "currentData": currentData
+      });
+    });
+  } else {
+
+    //var myDate = new Date(currentData.lastPumpActivation);
+    //currentData.lastPumpActivation = dateFormat(myDate, "h:MM:ss, dddd, mmmm dS");
 
     res.render('home', {
       "currentData": currentData
     });
-  });
+  }
 
   //var updatedData = db.get('plant_iot_log').find().sort({ lastPumpActivation: -1 }).limit(1);
   /*
@@ -26,6 +52,38 @@ router.get('/', function (req, res) {
   */
 });
 
+router.get('/start', function (req, res) {
+
+  gotStartRequest = true;
+
+  res.send();
+});
+
+router.get('/stop', function (req, res) {
+
+  gotStopRequest = true;
+
+  res.send();
+});
+
+router.get('/requests/start', function (req, res) {
+  if (gotStartRequest) {
+    res.status(200).send();
+    gotStartRequest = false;
+  } else {
+    res.status(404).send();
+  }
+});
+
+router.get('/requests/stop', function (req, res) {
+  if (gotStopRequest) {
+    res.status(200).send();
+    gotStopRequest = false;
+  } else {
+    res.status(404).send();
+  }
+});
+
 router.post('/addlog', function (req, res) {
 
   // Set our internal DB variable
@@ -35,30 +93,50 @@ router.post('/addlog', function (req, res) {
   var moisture = req.body.moisture;
   var isPumpOn = req.body.isPumpOn;
   var lastPumpActivation = req.body.lastPumpActivation;
+  var temperature = req.body.temperature;
 
   var utcSeconds = lastPumpActivation;
   var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
   d.setUTCSeconds(utcSeconds);
 
-  // Set our collection
-  var collection = db.get('plant_iot_log');
+  console.log(d.getTime() - lastSave.getTime() + ">" + 1 * 60 * 1000);
 
-  // Submit to the DB
-  collection.insert({
+  if (d.getTime() - lastSave.getTime() > 1 * 60 * 1000) {
+    console.log("saving minute log");
+    lastSave = d;
+    // Set our collection
+    var collection = db.get('plant_iot_log');
+
+    // Submit to the DB
+    collection.insert({
+      "moisture": moisture,
+      "isPumpOn": isPumpOn,
+      "lastPumpActivation": d,
+      "temperature": temperature,
+    }, function (err, doc) {
+      if (err) {
+        // If it failed, return error
+        res.send("There was a problem adding the information to the database.");
+      }
+    });
+  }
+
+  if (isPumpOn == true) {
+    isPumpOn = "En marche";
+  } else if (isPumpOn == false) {
+    isPumpOn = "À l'arrêt";
+  }
+
+  d = dateFormat(d, "h:MM:ss, dddd, mmmm dS");
+
+  currentData = {
     "moisture": moisture,
     "isPumpOn": isPumpOn,
     "lastPumpActivation": d,
-  }, function (err, doc) {
-    if (err) {
-      // If it failed, return error
-      res.send("There was a problem adding the information to the database.");
-    }
-    else {
-      // And forward to success page
-      res.send();
-    }
-  });
+    "temperature": temperature,
+  };
 
+  res.send();
 });
 
 module.exports = router;
